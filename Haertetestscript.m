3c11cb69faf8c6cce6303b830cea78ebdeae5870
stel1 <33962126+stel1@users.noreply.github.com>
2017-12-07 13:27:06 +0100

    Add files via upload

---
+++ Haertetestscript.m
@@ -0,0 +1,39 @@
+%Haertetest script für die Farberkennung
+
+durchlaeufe=1000000;
+fehler=0;
+
+for ii=1:durchlaeufe
+    [g1, colorName] = textColorOut();
+    [f]=scanCubeColour(g1);
+    switch f
+        case 1
+            zz='red';
+            if strcmp(colorName,zz) == 0
+                fehler=fehler+1;
+                
+            end
+                
+            
+        case 2
+            zz='green';
+            if strcmp(colorName,zz)== 0
+                fehler=fehler+1;
+            end
+            
+        case 3
+            zz='blue';
+            if strcmp(colorName,zz)== 0
+                fehler=fehler+1;
+            end
+            
+        case 0
+            zz='black';
+            if strcmp(colorName,zz)== 0
+                fehler=fehler+1;
+            end
+            
+    end
+end
+
+disp(['Bei ', num2str(durchlaeufe) ,' Durchläufen sind ' ,num2str(fehler),' Fehler aufgetreten.'])
